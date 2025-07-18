ca542f2a26da9bae27cdde817efb638a01c5c561
oneal <khalifaanil84@gmail.com>
2025-07-18 21:41:24 +0545

    Setup: Project Setup with requirements

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack:{
+    rules: {
+      '*.svg':{
+        loaders: ['@svgr/webpack'],
+        as: '*.js'
+      },
+    },
+  },
+  // experimental:{
+  //   typedRoutes: true,
+  // }
 };
 
 export default nextConfig;
